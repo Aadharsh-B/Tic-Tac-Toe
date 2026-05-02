68ba77056896dad41c42c5b75fcc04f371d64b69
Aadharsh Balasaravanan <aadharshbalasaravanan@gmail.com>
2026-05-02 18:55:14 +0530

    feat: Remove Unwanted Terminal Interfaces

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,5 @@
 from .core import XOGame
-from .graphical_interface import TerminalGame
 
 __all__ = [
     "XOGame",
-    "TerminalGame",
 ]
--- src/graphical_interface/__init__.py
+++
@@ -1,5 +0,0 @@
-from .terminal import TerminalGame
-
-__all__ = [
-    "TerminalGame",
-]
